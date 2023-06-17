4e221598e45367e5a65349cfbc1b9a6e38fd4757
Ming <85300101+yingming97@users.noreply.github.com>
2023-06-17 19:49:51 +0700

    //

--- app/src/main/java/pham/hien/barcodescanner/MainActivity.kt
+++ app/src/main/java/pham/hien/barcodescanner/MainActivity.kt
@@ -77,13 +77,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
 
-//        if (ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
-//        } else {
-//            startScanning()
-//        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+        } else {
+            startScanning()
+        }
     }
 
     private fun getQuickQRCode(
@@ -122,62 +122,62 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-//    private fun startScanning() {
-//        codeScanner = CodeScanner(this, binding.scannerView)
-//        codeScanner.camera = CodeScanner.CAMERA_BACK
-//        codeScanner.formats = CodeScanner.ALL_FORMATS
-//        codeScanner.autoFocusMode = AutoFocusMode.SAFE
-//        codeScanner.scanMode = ScanMode.SINGLE
-//        codeScanner.isAutoFocusEnabled = true
-//        codeScanner.isFlashEnabled = false
-//        codeScanner.decodeCallback = DecodeCallback {
-//            runOnUiThread {
-//                Snackbar.make(binding.root, "${it.text}", Snackbar.LENGTH_INDEFINITE)
-//                    .setAction("Ẩn") {
-//                        codeScanner.startPreview()
-//                    }.show()
-//            }
-//            Log.d(TAG, "startScanning: ${it.barcodeFormat}")
-//        }
-//        codeScanner.errorCallback = ErrorCallback {
-//            runOnUiThread {
-//                Toast.makeText(this, "Result${it.message}", Toast.LENGTH_LONG).show()
-//            }
-//        }
-//        binding.scannerView.setOnClickListener {
-//            codeScanner.startPreview()
-//        }
-//    }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray,
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == 1) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Đã cấp quyền", Toast.LENGTH_LONG).show()
-//                startScanning()
-//            } else {
-//                Toast.makeText(this, "Chưa có quyền", Toast.LENGTH_LONG).show()
-//            }
-//        }
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        if (::codeScanner.isInitialized) {
-//            codeScanner?.startPreview()
-//        }
-//    }
-//
-//    override fun onPause() {
-//        if (::codeScanner.isInitialized) {
-//            codeScanner?.releaseResources()
-//        }
-//        super.onPause()
-//    }
+    private fun startScanning() {
+        codeScanner = CodeScanner(this, binding.scannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Snackbar.make(binding.root, "${it.text}", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Ẩn") {
+                        codeScanner.startPreview()
+                    }.show()
+            }
+            Log.d(TAG, "startScanning: ${it.barcodeFormat}")
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Result${it.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+        binding.scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Đã cấp quyền", Toast.LENGTH_LONG).show()
+                startScanning()
+            } else {
+                Toast.makeText(this, "Chưa có quyền", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::codeScanner.isInitialized) {
+            codeScanner?.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        if (::codeScanner.isInitialized) {
+            codeScanner?.releaseResources()
+        }
+        super.onPause()
+    }
 
 
 }
